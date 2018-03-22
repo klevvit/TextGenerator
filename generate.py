5c9e95a3cf7ebc072d4a8f29ba36f870adf54c2c
klevvit <33697374+klevvit@users.noreply.github.com>
2018-03-22 19:16:39 +0300

    bugfix

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ with open('dict.txt', 'r') as f:
         tup = line.split(WORD_SEPARATOR)
         d[tup[0] + WORD_SEPARATOR + tup[1]] = int(tup[-1])
         pairCounter += int(tup[-1])
-        new_line = f.readline()
+        line = f.readline()
 
 # debug output
 print(pairCounter)
