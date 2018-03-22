471311e25dda14d0a3f244958fcbb108c0b48dac
klevvit <33697374+klevvit@users.noreply.github.com>
2018-03-22 18:59:25 +0300

    Started working with generator, added reading (loading) dict.txt

--- generate.py
+++ generate.py
@@ -1,6 +1,16 @@
 from collections import defaultdict
 
+WORD_SEPARATOR = ' '  # Const
 d = defaultdict(int)
 pairCounter = 0
 with open('dict.txt', 'r') as f:
+    line = f.readline()
+    while line != '':
+        tup = line.split(WORD_SEPARATOR)
+        d[tup[0] + WORD_SEPARATOR + tup[1]] = int(tup[-1])
+        pairCounter += int(tup[-1])
+        new_line = f.readline()
 
+# debug output
+print(pairCounter)
+print(d)
